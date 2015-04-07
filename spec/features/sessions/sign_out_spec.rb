96ccb999f8cc88382860a59acfd42cf75fc7a23f
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2015-04-07 01:56:30 -0300

    Add feature spec for user sign out

---
+++ spec/features/sessions/sign_out_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.feature "User sign out" do
+  let(:user) { create(:user, email: "john.doe@example.com") }
+
+  scenario "redirects to the home page" do
+    sign_in user
+
+    click_link "Logout"
+
+    expect(current_path).to eq root_path
+    expect(page).to have_notice "Logout efetuado com sucesso."
+    expect(page).to_not have_content "john.doe@example.com"
+  end
+end
