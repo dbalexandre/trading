9c1aaf0f9e3472ff2d5967ca59255547e70d00f9
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2015-04-07 01:53:38 -0300

    Add feature helper to deal with user sign in on specs

---
+++ spec/support/feature/helpers.rb
@@ -0,0 +1,20 @@
+module Feature
+  module Helpers
+    def sign_in(user)
+      visit new_user_session_path
+
+      fill_in "Email", with: user.email
+      fill_in "Password", with: user.password || "password"
+
+      click_button "Log in"
+    end
+
+    def sign_out
+      click_button "Sign out"
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.include Feature::Helpers, type: :feature
+end
