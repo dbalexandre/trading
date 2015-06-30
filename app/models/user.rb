507f4a70743e8cf80561ad0fd332476be9e6101d
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2015-06-29 22:20:36 -0300

    Add profile fields to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,27 @@
 class User < ActiveRecord::Base
+  extend Enumerize
+
   devise :database_authenticatable, :registerable, :recoverable,
          :rememberable, :trackable, :validatable
+
+  store_accessor :profile, :first_name, :last_name, :corporate_name,
+                           :cpf, :cnpj, :personable_type, :phone
+
+  enumerize :personable_type, in: [:company, :individual], default: :individual
+
+  validates :personable_type, :phone, presence: true
+  validates :first_name, :last_name, presence: true, if: :individual?
+  validates :corporate_name, presence: true, if: :company?
+  validates :cpf, cpf: true, if: :individual?
+  validates :cnpj, cnpj: true, if: :company?
+
+  private
+
+  def company?
+    personable_type.to_s.inquiry.company?
+  end
+
+  def individual?
+    personable_type.to_s.inquiry.individual?
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,18 @@ RSpec.describe User do
   describe "validations" do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password) }
+    it { should validate_presence_of(:personable_type) }
+    it { should validate_presence_of(:phone) }
 
     it { should validate_confirmation_of(:password) }
+
+    context "when is a company" do
+      it { should validate_presence_of(:corporate_name) }
+    end
+
+    context "when is an individual" do
+      it { should validate_presence_of(:first_name) }
+      it { should validate_presence_of(:last_name) }
+    end
   end
 end
