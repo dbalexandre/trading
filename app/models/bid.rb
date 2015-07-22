c90aab95e126ddcaae022f2f11836ac2f45b74c8
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2015-07-21 21:27:11 -0300

    Add payment term when payment type is forward

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -7,9 +7,15 @@ class Bid < ActiveRecord::Base
 
   validates :user, :product, :quantity, :price, :city, :state,
             :payment_type, :number_of_days, presence: true
+  validates :payment_term, presence: true, if: :forward?
 
   validates :number_of_days, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :payment_term, numericality: { only_integer: true, greater_than_or_equal_to: 1 }, if: :forward?
 
   scope :available, ->(user) { where.not(user: user) }
   scope :most_recent, -> { order(created_at: :desc) }
+
+  def forward?
+    payment_type.to_s.inquiry.forward?
+  end
 end
--- spec/models/bid_spec.rb
+++ spec/models/bid_spec.rb
@@ -15,6 +15,20 @@ RSpec.describe Bid do
     it { is_expected.to validate_presence_of(:state) }
     it { is_expected.to validate_presence_of(:user) }
 
+    context "when payment_type is cash" do
+      subject { described_class.new(payment_type: :cash) }
+
+      it { is_expected.to_not validate_presence_of(:payment_term) }
+      it { is_expected.to_not validate_numericality_of(:payment_term) }
+    end
+
+    context "when payment_type is forward" do
+      subject { described_class.new(payment_type: :forward) }
+
+      it { is_expected.to validate_presence_of(:payment_term) }
+      it { is_expected.to validate_numericality_of(:payment_term).only_integer.is_greater_than_or_equal_to(1) }
+    end
+
     it { is_expected.to validate_numericality_of(:number_of_days).only_integer.is_greater_than_or_equal_to(1) }
   end
 end
